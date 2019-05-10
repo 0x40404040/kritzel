18f91c4d03eb1e0807feff0d1184d784fbc7ea56
0x40404040 <0x40404040@protonmail.com>
2019-05-10 15:15:24 +0200

    removed preference window prototype

--- src/window.c
+++ src/window.c
@@ -60,34 +60,6 @@ static void window_cb_destroy(GtkWidget* widget, gpointer app_state) {
 	free(app_state);
 }
 
-static void test(GtkColorChooser* chooser, GdkRGBA* color, gpointer data) {
-	g_print("Test");
-}
-
-static void btn_settings_pressed(GtkWidget* widget, gpointer user_data) {
-	GtkWidget* grid_pref_color = gtk_grid_new();
-	GtkWidget* label_color_1 = gtk_label_new("Color 1");
-	GtkWidget* label_color_2 = gtk_label_new("Color 2");
-	GtkWidget* label_color_3 = gtk_label_new("Color 3");
-	GtkWidget* label_color_4 = gtk_label_new("Color 4");
-
-	gtk_grid_attach(GTK_GRID(grid_pref_color), label_color_1, 0, 0, 2, 1);
-	gtk_grid_attach(GTK_GRID(grid_pref_color), label_color_2, 0, 1, 2, 1);
-	gtk_grid_attach(GTK_GRID(grid_pref_color), label_color_3, 0, 2, 2, 1);
-	gtk_grid_attach(GTK_GRID(grid_pref_color), label_color_4, 0, 3, 2, 1);
-
-
-	GtkWidget* win_preferences = gtk_dialog_new();
-	gtk_window_set_title(GTK_WINDOW(win_preferences), "Preferences");
-	gtk_window_set_default_size(GTK_WINDOW(win_preferences), 600, 400);
-	g_signal_connect_swapped(win_preferences, "response", G_CALLBACK(gtk_widget_destroy), win_preferences);
-
-	GtkWidget* content_area = gtk_dialog_get_content_area(GTK_DIALOG(win_preferences));
-	gtk_container_add(GTK_CONTAINER(content_area), grid_pref_color);
-	
-	gtk_widget_show_all(win_preferences);
-}
-
 void window_init(GtkWidget* window) {
 
 	gtk_window_set_title(GTK_WINDOW(window), "kritzel - Drawing application");
@@ -100,62 +72,24 @@ void window_init(GtkWidget* window) {
 	canvas_init(overlay, app_state->selected_color, app_state->background_color, &app_state->selected_line_width);
 
 	GtkWidget* color_button = gtk_color_button_new_with_rgba(app_state->selected_color);
+	gtk_widget_set_size_request(color_button, 50, 50);
 	gtk_widget_set_halign(color_button, GTK_ALIGN_START);
 	gtk_widget_set_valign(color_button, GTK_ALIGN_START);
+
+	GtkCssProvider* provider = gtk_css_provider_new();
+	gtk_css_provider_load_from_data(provider,
+									"button {margin: 10px; border: transparent; background: none}\0", -1, NULL);
+	gtk_style_context_add_provider(gtk_widget_get_style_context(color_button),
+								   GTK_STYLE_PROVIDER(provider),
+								   GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+	gtk_style_context_add_class(gtk_widget_get_style_context(color_button), "circular");
+
+
 	g_signal_connect(color_button, "color-set", G_CALLBACK(color_button_cb_color_set), app_state->selected_color);
 	gtk_overlay_add_overlay(GTK_OVERLAY(overlay), color_button);
 
-
-
-
-		
-
-	GtkWidget* button_settings = gtk_button_new();
-	gtk_widget_set_halign(button_settings, GTK_ALIGN_START);
-	gtk_widget_set_valign(button_settings, GTK_ALIGN_END);
-	g_signal_connect(button_settings, "pressed", G_CALLBACK(btn_settings_pressed), NULL);
-	gtk_overlay_add_overlay(GTK_OVERLAY(overlay), button_settings);
-
-	// GtkWidget* button = gtk_button_new();
-	// gtk_widget_set_size_request(button, 50, 50);
-
-	// GtkCssProvider* provider = gtk_css_provider_new();
-	// gtk_css_provider_load_from_data(provider,
-	// 								"button {margin: 10px}\0", -1, NULL);
-	// gtk_style_context_add_provider(gtk_widget_get_style_context(button),
-	// 							   GTK_STYLE_PROVIDER(provider),
-	// 							   GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-
-
-	// gtk_widget_set_halign(button, GTK_ALIGN_START);
-	// gtk_widget_set_valign(button, GTK_ALIGN_START);
-	// gtk_style_context_add_class(gtk_widget_get_style_context(button), "circular");
-
-
-	// GtkWidget* ui_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-	// GtkCssProvider* ui_box_provider = gtk_css_provider_new();
-	// gtk_css_provider_load_from_data(ui_box_provider,
-	// 								"box {background-color: rgba(0, 0, 0, 0.1); padding-top: 30px; padding-bottom: 30px; padding-right: 10px; padding-left: 10px; border-radius: 20px}\n", -1, NULL);
-
-
-	// gtk_style_context_add_provider(gtk_widget_get_style_context(ui_box),
-	// 							   GTK_STYLE_PROVIDER(ui_box_provider),
-	// 							   GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-
-	// GtkWidget* color_button = gtk_color_button_new();
-
-	// gtk_style_context_add_class(gtk_widget_get_style_context(color_button), "circular");
-	// gtk_container_add(GTK_CONTAINER(ui_box), color_button);
-
-	// g_signal_connect(button, "button-press-event", G_CALLBACK(blub), ui_box);
-	// g_signal_connect(overlay, "get-child-position", G_CALLBACK(overlay_cb_get_child_position), NULL);
-	// g_signal_connect(color_button, "color-set", G_CALLBACK(color_button_cb_color_set), canvas_state);
-
-	// gtk_overlay_add_overlay(GTK_OVERLAY(overlay), button);
-	// gtk_overlay_add_overlay(GTK_OVERLAY(overlay), ui_box);
 	g_signal_connect(window, "destroy", G_CALLBACK(window_cb_destroy), app_state);
 
 
 	gtk_widget_show_all(window);
-	//gtk_widget_hide(ui_box);
 }
